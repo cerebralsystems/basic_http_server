5f71c11c5eee16804355c7c76cfea45006cc49b2
yang <yang@cerebralsystems.io>
2021-05-03 17:14:39 -0700

    first commit

---
+++ cerebral_http_server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Hello from Cerebral Systems!\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
+
+    err := http.ListenAndServe(":9001", nil)
+    if err != nil {
+        fmt.Println("ListenAndServe: ", err)
+    }
+}
